a4a46be6f959fe0e1cd8458f0217b173d690827f
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2021-05-07 10:05:25 +0200

    Java project template

---
+++ java/src/test/java/Setup.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Setup
+{
+	@Test()
+	public void shouldBeFine()
+	{
+		assertTrue(true);
+	}
+}
